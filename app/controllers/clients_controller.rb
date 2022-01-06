8eea0f95aa461ef11419f4ab92c5ea2ebddb6d8d
hardy-navadiya <hardynavadiya.tech@gmail.com>
2022-01-06 17:23:36 +0530

    client edit delete complete

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,18 +1,43 @@
 class ClientsController < ApplicationController
+	def index
+		@client = Client.new
+		@clients = Client.all
+	end
+
 	def new
 		@client = Client.new
 	end
 
 	def create
-	    @client = Client.new(client_params)
-	    if @client.save
-	    	flash[:success] = "Client created Successfully!!"
-	      redirect_to @client
-	    else
-	      render 'new'
-	    end
-  	end
+    @client = Client.new(client_params)
+    if @client.save
+    	flash[:success] = "Client created Successfully!!"
+      redirect_to @client
+    else
+      render 'new'
+    end
+	end
 
+	def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+  	@client = Client.find(params[:id])
+  	if @client.update(client_params)
+      flash[:success] = "Client detail edited!!"
+      redirect_to @client
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  	@client = Client.find_by(params[:id])
+  	@client.destroy
+  	flash[:danger] = "Client deleted!!"
+  	redirect_to @client
+  end
 
 	def show
 		@client = Client.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  delete 'destroyclient' => 'clients#destroy'
 
   root 'home#index'
 
