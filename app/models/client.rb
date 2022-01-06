b672609b592c57c1a993bec6d38487625e534f79
hardy-navadiya <hardynavadiya.tech@gmail.com>
2022-01-06 16:08:47 +0530

    validation in client

---
+++ app/models/client.rb
@@ -0,0 +1,6 @@
+class Client < ApplicationRecord
+	before_save { self.email = email.downcase }
+	regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :name, presence: true, length: {maximum: 40}
+	validates :email, presence: true, length: {maximum: 255}, format: { with: regex },uniqueness: { case_sensitive: false }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_101324) do
+ActiveRecord::Schema.define(version: 2022_01_06_103053) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
